69f5671792f6290e9e17cb68d9d9ebcf21d32de0
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-06-11 10:35:46 +0200

    pass function for printf converting to common file

--- applications/sam/src/sam/common/utility.hpp
+++ applications/sam/src/sam/common/utility.hpp
@@ -11,7 +11,26 @@ bool string_to_boolean( const std::string& str )
 	return ( str == "1" || boost::iequals(str, "y") || boost::iequals(str, "yes") || boost::iequals(str, "true") );
 }
 
-
+void convertprintfStyleToUsual( std::string& sequenceName )
+{
+	for( size_t i=0; i< sequenceName.length(); i++ )
+	{
+		if( sequenceName.at(i) == '%' )
+		{
+			if( sequenceName.at( i + 3 ) == 'd' )
+			{
+				std::string value = sequenceName;
+				value.erase( i + 3, value.length() - i - 3 );
+				value.erase( 0, i + 1 );
+				if( isdigit( value.at( 0 ) ) && isdigit( value.at( 1 ) ) )
+				{
+					sequenceName.erase( i, 4 );
+					sequenceName.insert( i, atoi( value.c_str() ), '#' );
+				}
+			}
+		}
+	}
+}
 
 #endif
 
