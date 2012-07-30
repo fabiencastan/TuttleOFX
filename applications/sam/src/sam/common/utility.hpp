8d11d4b090cb24c4be9fbb3abb3dce35c87dc626
Marc-Antoine ARNAUD <arnaud.marcantoine@gmail.com>
2012-07-30 18:03:16 +0200

    adding detection for @ %0nd and # pattern

--- applications/sam/src/sam/common/utility.hpp
+++ applications/sam/src/sam/common/utility.hpp
@@ -4,33 +4,69 @@
 #include <boost/algorithm/string/predicate.hpp>
 
 #include <string>
-
+#include <iostream>
 
 bool string_to_boolean( const std::string& str )
 {
 	return ( str == "1" || boost::iequals(str, "y") || boost::iequals(str, "yes") || boost::iequals(str, "true") );
 }
 
-void convertprintfStyleToUsual( std::string& sequenceName )
+std::string convertprintfStyleToUsual( const std::string& sequenceName )
 {
-	for( size_t i=0; i< sequenceName.length(); i++ )
+	std::string sequence = sequenceName;
+	for( size_t i = 0; i < sequence.length(); i++ )
 	{
-		if( sequenceName.at(i) == '%' )
+		if( sequence.at(i) == '%' )
 		{
-			if( sequenceName.at( i + 3 ) == 'd' )
+			if( sequence.at( i + 3 ) == 'd' )
 			{
-				std::string value = sequenceName;
+				std::string value = sequence;
 				value.erase( i + 3, value.length() - i - 3 );
 				value.erase( 0, i + 1 );
 				if( isdigit( value.at( 0 ) ) && isdigit( value.at( 1 ) ) )
 				{
-					sequenceName.erase( i, 4 );
-					sequenceName.insert( i, atoi( value.c_str() ), '#' );
+					sequence.erase( i, 4 );
+					sequence.insert( i, atoi( value.c_str() ), '#' );
 				}
 			}
 		}
 	}
+	return sequence;
 }
+
+std::string removeSharpAndArobase( const std::string& sequenceName )
+{
+	std::string sequence = sequenceName;
+	for( size_t i = 0; i < sequence.length(); i++ )
+	{
+		if( sequence.at(i) == '#' || sequence.at(i) == '@' )
+		{
+			sequence.erase( i, 1 );
+			i--;
+		}
+	}
+	return sequence;
+}
+
+bool sequenceNamesMatch( const std::string& sequenceA, const std::string& sequenceB )
+{
+	if( strcmp( convertprintfStyleToUsual( sequenceA ).c_str(), convertprintfStyleToUsual( sequenceB ).c_str() ) == 0 )
+		return true;
+	
+	//std::cout << removeSharpAndArobase( sequenceA ).c_str() << std::endl;
+	//std::cout << removeSharpAndArobase( sequenceB ).c_str() << std::endl;
+	
+	size_t foundA = sequenceA.find( '@' );
+	size_t foundB = sequenceB.find( '@' );
+	if( foundA != std::string::npos || foundB != std::string::npos )
+	{
+		if( strcmp( removeSharpAndArobase( sequenceA ).c_str(), removeSharpAndArobase( sequenceB ).c_str() ) == 0 )
+			return true;
+	}
+	
+	return false;
+}
+
 
 #endif
 
